80445a1aa678a712ecf04ad0440702f35129be42
snowsecure <philip.snowden@gmail.com>
2025-12-20 21:21:26 -0600

    Add Sail emoji branding with info modal

---
+++ src/lib/customScenario.ts
@@ -0,0 +1,8 @@
+export interface CustomScenarioDetails {
+    title: string;
+    context: string;
+    issue: string;
+    employeeName: string;
+    employeeRole: string;
+    personaTraits: string;
+}
